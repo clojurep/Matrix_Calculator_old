d5298e90b0a2b7d48e3c643a58317647ed0b8dff
Clojure Project <clojurep@gmail.com>
2013-01-23 10:48:46 +0200

    Added input conditions to all functions

--- src/Matrix_Calculator.clj
+++ src/Matrix_Calculator.clj
@@ -6,32 +6,34 @@
 
 
 (defn sumVector [vec1]
+  {:pre [(vector? vec1)] }
   
-  (apply + vec1)
+   (apply + vec1)
  )
 
 (defn sumMatrix [mat1]
+  {:pre [(matrix? mat1)] }
   
-  (apply + (apply concat mat1))
+   (apply + (apply concat mat1))
   )
 
 (defn addVectors [vec1 vec2]
-    {:pre [(= (count vec1) (count vec2)) ]
-   }
-   (vec 
-     (lazy-seq
-       (when-let [v1 (seq vec1)]
-         (when-let [v2 (seq vec2)]
-           (map + v1 v2)
-       )
+    {:pre [(vector? vec1) (vector? vec2) (= (count vec1) (count vec2)) ] }
+   
+    (vec 
+      (lazy-seq
+        (when-let [v1 (seq vec1)]
+          (when-let [v2 (seq vec2)]
+            (map + v1 v2)
       )
      )
     )
    )
+  )
 
 (defn addMatrixes [mat1 mat2]
-  {:pre [(= (count mat1) (count mat2)) ]
-   }
+  {:pre [(sameSize? mat1 mat2)] }
+   
    (vec 
      (lazy-seq
        (when-let [m1 (seq mat1)]
@@ -45,16 +47,15 @@
  
 
 (defn createMultipicationVec [vec1 vec2]
-    {:pre [(= (count vec1) (count vec2)) ]
-   }
-      (map * vec1 vec2)
+    {:pre [(vector? vec1) (vector? vec2) (= (count vec1) (count vec2)) ]  }
+     
+     (map * vec1 vec2)
  )
 
 (defn dotProduct [vec1 vec2]
-    {:pre [(= (count vec1) (count vec2)) ]
-  }
+    {:pre [(vector? vec1) (vector? vec2) (= (count vec1) (count vec2)) ]  }
     
-    (apply + (map * vec1 vec2))
+     (apply + (map * vec1 vec2))
 )
 
 
@@ -64,65 +65,72 @@
 
 
 (defn vecScalarMul [vec1 s]
- 
-  (lazy-seq
-    (when-let [v1 (seq vec1)]
-      (map (mulBy s) v1)
-   )
-  )
-)
-
-(defn matScalarMul [mat1 s]
-  
+  {:pre [(vector? vec1)] }
+   
    (lazy-seq
-     (when-let [m1 (seq mat1)]
-         (map #(vecScalarMul % s) m1)
+     (when-let [v1 (seq vec1)]
+       (map (mulBy s) v1)
     )
    )
  )
 
-
-(defn subMatrixes [mat1 mat2]
-  
-  (addMatrixes mat1 (matScalarMul mat2 -1)) 
- )
-
-
-(defn matTranspose [mat1]
-  (vec (apply map vector mat1))
- )
-
-
-
-(defn matMultipication [mat1 mat2]
-  
-  (if (= (count mat1) 0) 
-  
-    nil
-    
-  (vec
-    (loop [rslt (vector) m1 mat1]
-      (if (= (count m1) 0)
-           rslt
-      
-           (recur (conj rslt (vec (map #(dotProduct (first m1) %) (matTranspose mat2)))) (rest m1))
-        )      
-       )
+(defn matScalarMul [mat1 s]
+  {:pre [(matrix? mat1)] }
+   
+   (lazy-seq
+      (when-let [m1 (seq mat1)]
+          (map #(vecScalarMul % s) m1)
      )
    )
  )
 
 
-(defn matMultipication_lazy [mat1 mat2]
+(defn subMatrixes [mat1 mat2]
+  {:pre [(sameSize? mat1 mat2)] }
   
-  (if (= (count mat1) 0) 
+   (addMatrixes mat1 (matScalarMul mat2 -1)) 
+ )
+
+
+(defn matTranspose [mat1]
+  {:pre [(matrix? mat1)] }
+   
+   (vec (apply map vector mat1))
+ )
+
+
+
+(defn matMultipication [mat1 mat2]
+  {:pre [(sameColsRows? mat1 mat2)] }
   
-    nil    
+   (if (= (count mat1) 0) 
+  
+      nil
     
-    (lazy-seq
-      (when-let [m1 mat1]
-        (when-let [m2 mat2]
-         (cons (map #(dotProduct (first m1) %) (matTranspose m2)) (matMultipication_lazy (rest m1) m2)) 
+     (vec
+       (loop [rslt (vector) m1 mat1]
+         (if (= (count m1) 0)
+            rslt
+      
+            (recur (conj rslt (vec (map #(dotProduct (first m1) %) (matTranspose mat2)))) (rest m1))
+      )      
+     )
+    )
+   )
+  )
+
+
+(defn matMultipication_lazy [mat1 mat2]
+  {:pre [(sameColsRows? mat1 mat2)] }
+  
+   (if (= (count mat1) 0) 
+  
+     nil    
+    
+     (lazy-seq
+       (when-let [m1 mat1]
+         (when-let [m2 mat2]
+          (cons (map #(dotProduct (first m1) %) (matTranspose m2)) (matMultipication_lazy (rest m1) m2)) 
       )
      )
     )
@@ -131,15 +139,16 @@
  
 
 (defn matPow [mat p]
-  (if (= p 1) 
-    mat
+  {:pre [(sameColsRows? mat mat)] }
+    (if (= p 1) 
+      mat
     
-    (if (even? p)
-      (let [rslt (matPow mat (/ p 2))] 
-        (matMultipication rslt rslt)
-        )
+      (if (even? p)
+        (let [rslt (matPow mat (/ p 2))] 
+          (matMultipication rslt rslt)
+         )
       
-      (matMultipication (matPow mat (- p 1)) mat)   
+        (matMultipication (matPow mat (- p 1)) mat)   
     )  
    )
   )
@@ -150,3 +159,27 @@
   ([f m1 m2] (f m1 m2))
   ([f m1 m2 & more] (reduce f (f m1 m2) more))
  )
+
+
+(defn matrix? [mat]
+  (and 
+    (vector? mat)
+    (apply = true (map vector? mat))
+    (apply = (map #(= (count (first mat)) (count %)) mat))
+    )
+  )
+
+(defn sameSize? [mat1 mat2]
+  {:pre [(matrix? mat1) (matrix? mat2)] }
+   
+   (and
+     (= (count mat1) (count mat2))
+     (= (count (first mat1)) (count (first mat2)))
+    )
+  ) 
+
+(defn sameColsRows? [mat1 mat2]
+  {:pre [(matrix? mat1) (matrix? mat2)] }
+    
+    (= (count (first mat1)) (count mat2))
+  )
